0e57afd8eba7f7300b362c209cb9da3a4dccaa95
howardmann <howardmann27@gmail.com>
2017-06-03 20:42:48 +1000

    initial commit

---
+++ node-app/src/server.js
@@ -0,0 +1,61 @@
+var express = require('express');
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var pg = require('pg');
+
+var votes = {
+  sandwiches: 0,
+  tacos: 0
+}
+
+let client = new pg.Client('postgres://postgres@172.17.0.1:9000/postgres');
+
+client.connect(function(err){
+  if(err) throw err;
+  client.query("select number_of_votes from votes where option_name='sandwiches'", function(err, result){
+    if (err) throw err;
+    votes.sandwiches = result.rows[0].number_of_votes;
+  });
+  client.query("select number_of_votes from votes where option_name='tacos'", function(err, result){
+    if (err) throw err;
+    votes.tacos = result.rows[0].number_of_votes;
+  });
+})
+
+var urlencodedParser = bodyParser.urlencoded({extended:false});
+
+var app = express();
+app.set('views', __dirname + '/views');
+// Express handlebars view engine
+app.engine('handlebars', exphbs({defaultLayout: 'main', layoutsDir: 'src/views/layouts'}));
+app.set('view engine', 'handlebars');
+
+
+// Routes
+app.get('/', function (req, res, next) {
+    res.render('home', {
+      votes: votes
+    });
+});
+
+app.post('/vote', urlencodedParser, function(req, res) {
+  var vote = req.body.yourVote;
+  if(vote === 'sandwiches') {
+    votes.sandwiches = votes.sandwiches + 1;
+  } else if(vote === 'tacos') {
+    votes.tacos = votes.tacos + 1;
+  } else {
+    console.log('Something went wrong: vote contains ' + vote);
+  }
+  res.redirect('/');  
+});
+
+// 404
+app.use(function(req, res){
+  res.status(404).send('404');
+});
+
+// Listen on port 3000
+app.listen(3000, function(){
+  console.log('Listening port 3000');
+});
